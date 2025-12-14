d57492579df96979d54b358f4d144683ab3195ff
SuperGoon97 <149195957+SuperGoon97@users.noreply.github.com>
2025-12-14 15:33:33 +0000

    particles

--- template/test_omni_light.gd
+++ template/test_omni_light.gd
@@ -1,7 +1,13 @@
 @tool class_name CustomOmniLight3D extends OmniLight3D
 
+@export var process_in_editor:bool = false:
+	set (value):
+		refresh_defaults()
+		process_in_editor = value
+
 @export var light_hot_cold:Gradient
 @export var easing_line:Curve
+@export var default_light_color:Color = Color.WHITE
 @export_range(0.0,1.0,0.01) var range_varience:float = 0.1
 @export_range(0.0,1.0,0.01) var attenuation_varience:float = 0.1
 @export_range(0.0,1.0,0.01) var energy_varience:float = 0.1
@@ -11,13 +17,22 @@
 @onready var default_range:float = omni_range
 @onready var default_attenuation:float = omni_attenuation
 @onready var default_energy:float = light_energy
-@onready var default_color:Color = light_color
+@onready var default_color:Color = default_light_color
 
 @onready var light_alpha:float = randf_range(0.0,1.0)
 var eased_light_alpha:float
 var direction:float = 1.0
 
+func refresh_defaults():
+	default_range = omni_range
+	default_attenuation = omni_attenuation
+	default_energy = light_energy
+	default_color = light_color
+
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		if process_in_editor == false:
+			return
 	var clamped_alpha = clampf(light_alpha,0.0,1.0)
 	eased_light_alpha = easing_line.sample(clamped_alpha)
 	omni_range = default_range + lerpf(-range_varience,range_varience,eased_light_alpha)
@@ -27,7 +42,7 @@ func _process(delta: float) -> void:
 	if light_hot_cold:
 		var sampled_hue:float = light_hot_cold.sample(clamped_alpha).h
 		if sampled_hue == 0.0: sampled_hue = 1.0
-		light_color = default_color.blend(light_hot_cold.sample(clamped_alpha)*0.33)
+		light_color = default_color.blend(light_hot_cold.sample(clamped_alpha)*1.0)
 	
 	if light_alpha > 1.0:
 		direction = -1
