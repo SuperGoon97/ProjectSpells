2c7e11b40f52d46f8b189db3e289b9f0a0e18c40
SuperGoon97 <149195957+SuperGoon97@users.noreply.github.com>
2026-01-13 17:02:52 +0000

    smol ui

--- template/Scripts/Globals/g_out.gd
+++ template/Scripts/Globals/g_out.gd
@@ -4,6 +4,12 @@ var out_dict:Dictionary[String,TextOutputBox]
 
 func out(target:String,string:String) -> void:
 	if out_dict[target]:
-		out_dict[target].add_line(string)
+		out_dict["all_debug"].add_line(string)
+		match target:
+			"debug":
+				out_dict[target].add_line(string)
+			_:
+				out_dict[target].add_line(string)
+				out_dict["all"].add_line(string)
 	else:
 		push_error("GOut out %s does not exist" %target)
--- template/Scripts/UI/Menu/ui_main.gd
+++ template/Scripts/UI/Menu/ui_main.gd
@@ -1,11 +1,19 @@
 class_name UiMain
 extends Control
 
+@export var outputbox_array:Array[TextOutputBox]
+
 @onready var control: Control = $Control
+@onready var option_button: OptionButton = $OptionButton
 
 func _game_ready() -> void:
 	GVar.signal_bus.option_change.connect(_option_changed)
-	pass
+	setup_textoutput()
+
+func setup_textoutput() -> void:
+	for output in outputbox_array:
+		output.visible = false
+	outputbox_array[option_button.selected].visible = true
 
 func _option_changed(option_string:String,value) -> void:
 	if option_string == "toggle_ui":
@@ -21,3 +29,9 @@ func toggle_visibility(value:int = -1) -> void:
 			visible = true
 		_:
 			print("invalid")
+
+func _on_option_button_item_selected(index: int) -> void:
+	for output in outputbox_array:
+		output.visible = false
+	outputbox_array[index].visible = true
+	pass # Replace with function body.
